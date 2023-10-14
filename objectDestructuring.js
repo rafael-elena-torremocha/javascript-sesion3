83a3f10d3041ac2909a1b670826219282343e0c5
rafael-elena-torremocha <torremocha.rafael@gmail.com>
2023-10-14 13:27:36 +0200

    ObjectDestructuring

---
+++ objectDestructuring.js
@@ -0,0 +1,32 @@
+/*
+    OBJECT DESTRUCTURING
+    
+    We can substract the value of any part of the objects.
+
+    For example:
+*/
+
+let marvelCharacter = {
+	name: {
+		heroName: 'Doctor Strange',
+		humanName:'Stephen Vincent Strange'
+	},
+	team: ['Avengers', 'Iluminati']
+}
+ debugger
+// Destructuring - Ex1
+let { name, team } = marvelCharacter;
+
+// Destructuring - Ex2
+// We can changes the name.
+let { name: nombre, team: equipo } = marvelCharacter;
+
+// Podemos acceder a las propiedades sin el marvelCharacter -> ya asignado.
+console.log(nombre.heroName, nombre.humanName);
+console.log(team[0], team[1]);
+
+// Destructuring - Ex2
+let { heroName, humanName } = marvelCharacter.name;
+
+console.log(heroName);
+console.log(humanName);
