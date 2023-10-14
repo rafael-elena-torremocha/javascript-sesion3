8585a68efd248b6de2bbbd98fe35ba1cfb2a9c0b
rafael-elena-torremocha <torremocha.rafael@gmail.com>
2023-10-14 18:59:09 +0200

    filter

---
+++ filter.js
@@ -0,0 +1,20 @@
+
+var pilots = [
+    { id: 2, name: "Wedge Antilles", faction: "Rebels", },
+    { id: 8, name: "Ciena Ree", faction: "Empire", },
+    { id: 40, name: "Iden Versio", faction: "Empire", },
+    { id: 66, name: "Thane Kyrell", faction: "Rebels", }
+];
+
+var rebels = pilots.filter(function (pilot) {
+    return pilot.faction === "Rebels";
+});
+
+console.log(rebels);
+
+var empire = pilots.filter(function (pilot) {
+    return pilot.faction === "Empire";
+});
+
+console.log(empire);
+
